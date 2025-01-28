bc1867fb60263356cd938d53aa3597ac226fa268
wthsoaresw <thiagosoares72474877@gmail.com>
2025-01-28 01:12:24 +0000

    codigo de cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -20,3 +20,31 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;}
+    Estado: A
+Código da Carta: A01
+Nome da Cidade: São Paulo
+População: 12325000
+Área: 1521.11 km²
+PIB: 699.28 bilhões de reais
+Número de Pontos Turísticos: 50
+
+Código da Carta: A02
+Nome da Cidade: Rio De Janeiro
+População:6.72.984
+Área: 1.255,3 Km²
+PIB: 331,28 bilhões de reais
+Número de Pontos Turísticos: 58
+
+Código da Carta: A03
+Nome da Cidade: Ribeião Pires
+População: 118.877
+Área: 98,972 Km²
+PIB: 3,021 milhões de reais
+Número de Pontos Turísticos: 11
+
+Código da Carta: A04
+Nome da Cidade: São José Dos Campos
+População: 697.054
+Área: 1.100 Km²
+PIB: 45.208 bilhões de reais
+Número de Pontos Turísticos: 20
