04b0a09a6c4b7d928339d9df7322289b67dbb446
wthsoaresw <thiagosoares72474877@gmail.com>
2025-02-19 23:22:48 +0000

    criei conmit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,50 +1,17 @@
 #include <stdio.h>
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    printf("desafio trunfo!1");
-    printf("novo conmit!1")
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
+    int numero, i;
+
+    printf("digite um numero para saber a tabuada...");
+    scanf("%d", &numero);
+
+    for (i = 0; i <= 10; i++)
+    {
+        printf("%d x %d = %d \n", i, numero, i * numero);
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    }
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
-    return 0;}
-    Estado: A
-Código da Carta: A01
-Nome da Cidade: São Paulo
-População: 12325000
-Área: 1521.11 km²
-PIB: 699.28 bilhões de reais
-Número de Pontos Turísticos: 50
-
-Código da Carta: A02
-Nome da Cidade: Rio De Janeiro
-População:6.72.984
-Área: 1.255,3 Km²
-PIB: 331,28 bilhões de reais
-Número de Pontos Turísticos: 58
-
-Código da Carta: A03
-Nome da Cidade: Ribeião Pires
-População: 118.877
-Área: 98,972 Km²
-PIB: 3,021 milhões de reais
-Número de Pontos Turísticos: 11
-
-Código da Carta: A04
-Nome da Cidade: São José Dos Campos
-População: 697.054
-Área: 1.100 Km²
-PIB: 45.208 bilhões de reais
-Número de Pontos Turísticos: 20
+   
+   return 0; 
+}
